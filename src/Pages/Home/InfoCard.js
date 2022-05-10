d51295412999061fdceec80ddde5d3a5c3cbb490
faisalahmed7 <faisalahmeddiu@gmail.comm>
2022-05-10 23:59:47 +0600

    Contact and Footer Added

--- src/Pages/Home/InfoCard.js
+++ src/Pages/Home/InfoCard.js
@@ -2,13 +2,13 @@ import React from 'react';
 
 const InfoCard = ({ img, cardTitle, bgClass }) => {
     return (
-        <div class={`card lg:card-side bg-base-100 shadow-xl ${bgClass}`}>
+        <div className={`card lg:card-side bg-base-100 shadow-xl ${bgClass}`}>
             <figure className='pl-6'>
                 <img src={img} />
             </figure>
-            <div class="card-body text-white">
-                <h2 class="card-title ">{cardTitle}</h2>
-                <p>Click the button to listen on Spotiwhy app.</p>
+            <div className="card-body text-white">
+                <h2 className="card-title ">{cardTitle}</h2>
+                <p>Click the button to listen on Spotify app.</p>
 
             </div>
         </div>
